e41a3cda46aeea22cf0905923a7512aa2fa8a3e9
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2024-08-04 15:36:18 +0100

    add some enahncement to proeject section

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled, { keyframes, css } from 'styled-components';
 import { useInView } from 'react-intersection-observer';
-import { FaChevronRight, FaChevronLeft, FaTimes } from 'react-icons/fa';
+import { FaChevronRight, FaChevronLeft, FaTimes, FaFolder, FaFolderOpen } from 'react-icons/fa';
 
 const ProjectSectionWrapper = styled.section`
   min-height: 100vh;
@@ -110,23 +110,41 @@ const fadeIn = keyframes`
 
 const ProjectList = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, auto);
+  gap: 15px;
+  padding: 10px;
 `;
 
 const ProjectItem = styled.div`
-  padding: 5px;
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  background-color: rgba(135, 206, 250, 0.1);
+  border-radius: 5px;
   cursor: pointer;
   animation: ${fadeIn} 0.5s ease-out forwards;
   animation-delay: ${props => props.index * 0.1}s;
   opacity: 0;
+  transition: all 0.3s ease;
+
   &:hover {
-    color: #4EC9B0;
+    background-color: rgba(135, 206, 250, 0.2);
   }
-  ${props => props.active && `
-    color: #4EC9B0;
-    font-weight: bold;
+
+  ${props => props.active && css`
+    background-color: rgba(78, 201, 176, 0.2);
+    box-shadow: 0 0 10px rgba(78, 201, 176, 0.3);
   `}
+`;
+
+const ProjectIcon = styled.span`
+  margin-right: 10px;
+  color: #4EC9B0;
+`;
+
+const ProjectName = styled.span`
+  flex-grow: 1;
 `;
 
 const ProjectDetails = styled.div`
@@ -254,11 +272,6 @@ const projects = [
       'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
       'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
       'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
-      'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
-      'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
-      'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
-      'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
-      'https://i.pinimg.com/564x/01/2b/0b/012b0bc2e871fc073c8dbf8008bdf20e.jpg',
     ]
   },
   // Add more projects as needed
@@ -301,7 +314,10 @@ function ProjectSection() {
                   onClick={() => handleProjectClick(project)}
                   index={index}
                 >
-                  {project.name}
+                  <ProjectIcon>
+                    {selectedProject && project.id === selectedProject.id ? <FaFolderOpen /> : <FaFolder />}
+                  </ProjectIcon>
+                  <ProjectName>{project.name}</ProjectName>
                 </ProjectItem>
               ))}
             </ProjectList>
@@ -366,7 +382,11 @@ function ProjectSection() {
               />
             ))}
           </ImageThumbnails>
-         
+          {project.images.length > 3 && !showAllThumbnails && (
+            <ShowMoreButton onClick={() => setShowAllThumbnails(true)}>
+              Show More
+            </ShowMoreButton>
+          )}
         </ProjectDetails>
       ));
       scrollToBottom();
@@ -425,7 +445,7 @@ function ProjectSection() {
             src={selectedProject.images[currentImageIndex]} 
             alt={`${selectedProject.name} screenshot`} 
           />
-          <ImageNavButton 
+       <ImageNavButton 
             onClick={() => handleImageChange(1)} 
             disabled={currentImageIndex === selectedProject.images.length - 1}
           >
